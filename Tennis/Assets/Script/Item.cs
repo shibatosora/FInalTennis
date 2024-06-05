d6b32683cfda3abc08b2154eb2fff27a346f64a7
sora <4810sora01@gmail.com>
2024-06-06 01:02:58 +0900

    06060100

---
+++ Tennis/Assets/Script/Item.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+    public enum ID
+    {
+        Card =0,
+        Enemy=1,
+        Apple =2,
+        Boss =3,
+    }
+    [SerializeField]
+    public class Item :MonoBehaviour
+    {
+        [SerializeField] GameObject itemObject;
+        [SerializeField] private ID ids;
+        
+        public GameObject ItemObject { get => itemObject; }
+        public ID Ids { get => ids; }
+        
+        
+    }
+}
+
---
+++ Tennis/Assets/Script/ItemBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace Script
+{
+    [CreateAssetMenu]
+    public class ItemBase :ScriptableObject
+    {
+        public List<Item> itemList = new List<Item>();
+
+
+    }
+}
